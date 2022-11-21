13eefecb67afe96d8e3c1b9a98dbbe9b9a828329
shengxi <2766274062@qq.com>
2022-11-21 12:08:31 +0800

    fix

--- src/tools/cookie.js
+++ src/tools/cookie.js
@@ -1,26 +1,4 @@
 export default {
-<<<<<<< HEAD
-    setCookie(cname, cvalue, exdays){
-        var d = new Date();
-        d.setTime(d.getTime()+(exdays*24*60*60*1000));
-        var expires = "expires="+d.toGMTString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    },
-    getCookie(cname){
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0; i<ca.length; i++)
-        {
-            var c = ca[i].trim();
-            if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-        }
-        return null;
-    },
-    removeCookie(cname){
-        this.setCookie(cname, '', -1);
-    }
-}
-=======
   setCookie(cname, cvalue, exdays) {
     var d = new Date()
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
@@ -40,4 +18,3 @@ export default {
     this.setCookie(cname, '', -1)
   },
 }
->>>>>>> 235c9f042e7a1074c9cef0bb3cddcb4f9c865708
--- src/tools/formatTime.js
+++ src/tools/formatTime.js
@@ -1,18 +1,4 @@
 import moment from 'moment'
-<<<<<<< HEAD
-moment.locale('zh-cn');
-function formatTime(date) {
-    let formatDate = new Date(date).getTime()
-    let time = new Date().getTime() - formatDate
-    if (time < 259200000) {
-        return moment(date).fromNow();
-    } else if (time > 259200000) {
-        return moment(date).format('YYYY-MM-DD')
-    }
-};
-
-export default  formatTime
-=======
 moment.locale('zh-cn')
 function formatTime(date) {
   let formatDate = new Date(date).getTime()
@@ -25,4 +11,3 @@ function formatTime(date) {
 }
 
 export default formatTime
->>>>>>> 235c9f042e7a1074c9cef0bb3cddcb4f9c865708
